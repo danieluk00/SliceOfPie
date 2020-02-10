f846ef4989673b2e20e1bfb4cc0517de047f0762
DANIEL LANDES <daniellandes@DANIELs-MacBook-Air.local>
2020-02-10 14:05:14 +0000

    Reduced graph caption length

--- scripts/app.js
+++ scripts/app.js
@@ -530,7 +530,12 @@ const barChart = object => {
 
         dates.push(keys[i].substring(0,3))
         sum.push(value)
-    }       
+    }
+
+    if (keys.length >=7)
+    for (let i=0; i<=keys.length; i++) {
+        dates[i] = dates[i].substring(0,1)
+    }
 
     let runningTotal = sum[0];
     for (let i=1; i<=keys.length; i++) {
